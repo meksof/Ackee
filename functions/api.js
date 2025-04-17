9395b54e7b55443e08315b56b818dcaf79e53bfe
Sofiane MEKSI <sof@meksi.me>
2025-04-17 12:08:01 +0200

    Refactor API to use new Ackee initialization method with MongoDB connection

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,12 @@
-const { createServer } = require('ackee-tracker');
+// Use the new Ackee initialization method
+const { Ackee } = require('ackee-tracker');
 
-module.exports = createServer({
-  database: {
-    url: process.env.ACKEE_MONGODB // Use environment variable
-  }
-});
+exports.handler = async (event, context) => {
+  const ackee = new Ackee({
+    database: {
+      url: process.env.ACKEE_MONGODB // MongoDB Atlas connection string
+    }
+  });
+
+  return ackee.server(event, context);
+};
